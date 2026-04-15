31d3ab8d933b9d29ccb0c2f67a27067a69ae11bf
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-04-15 12:12:16 +0200

    update: add function createTaskElement + add completed task section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // LIST OF TASKS
 let taskList = [
     { name: "Go shopping", isCompleted: false, isEditing: false, priority: "low" },
-    { name: "Go to the gym", isCompleted: false, isEditing: false, priority: "medium" },
+    { name: "Go to the gym", isCompleted: true, isEditing: false, priority: "medium" },
     { name: "Finish work", isCompleted: false, isEditing: false, priority: "high" },
 ];
 
@@ -14,35 +14,96 @@ let checkShow = false;
 let addNewTaskBtn = document.querySelector('#addNewTaskBtn');
 let addNewTaskInput = document.querySelector('#addNewTaskInput'); 
 
+
 // FUNCTIONS
-function renderList(){
-    let tasks = ``;
-    taskList.forEach((task) => {
-        tasks += `
-            <div class="row justify-content-center align-items-center rowTask">
-                <div class="col-8">
-                    <h4 class="m-0">${task.name}</h4>
-                    <p class="d-inline me-4 text-uppercase fs-6">Priority: ${task.priority}</p>
-                </div>
-                <div class="col-4 col-md-2 d-flex justify-content-around align-items-center p-0">
-                    <i id="iconComplete" class="icon bi bi-check-lg fs-3" title="Mark as completed"></i>
-                    <i id="iconModify" class="icon bi bi-pencil-fill" title="Modify your task"></i>
-                    <i id="iconDelete" class="icon bi bi-trash3-fill" title="Delete your task"></i>
-                </div>
-            </div>
-        `
-    })
+function createTaskElement(task, index){
+    let row = document.createElement('div');
+    row.classList.add('row', 'justify-content-center', 'align-items-center', 'rowTask');
     
-    wrapperList.innerHTML = `
-        <header>
-            <h2 class="text-center display-6 mb-3 text-sg fw-bold">YOUR TO-DO LIST</h2>
-        </header>
-        ${tasks}
-    `
+    let col1 = document.createElement('div');
+    col1.classList.add('col-8');
+    
+    let title = document.createElement('h4');
+    title.classList.add('m-0');
+    title.textContent = task.name;
+    
+    let priority = document.createElement('p');
+    priority.classList.add('d-inline', 'me-4', 'text-uppercase', 'fs-6');
+    priority.textContent = `Priority: ${task.priority}`;
+
+    col1.append(title, priority);
+
+    let col2 = document.createElement('div');
+    col2.classList.add('col-4', 'col-md-2', 'd-flex', 'justify-content-around', 'align-items-center', 'p-0');
+
+    let deleteIcon = document.createElement('i');
+    deleteIcon.classList.add('iconDelete', 'icon', 'bi', 'bi-trash3-fill');
+    deleteIcon.title = 'Delete your task';
+
+    if(!task.isCompleted){
+        let completeIcon = document.createElement('i');
+        completeIcon.classList.add('iconComplete', 'icon', 'bi', 'bi-check-lg', 'fs-3');
+        completeIcon.title = 'Mark as completed';
+    
+        let editIcon = document.createElement('i');
+        editIcon.classList.add('iconEdit', 'icon', 'bi', 'bi-pencil-fill');
+        editIcon.title = 'Edit your task';
+    
+        col2.append(completeIcon, editIcon, deleteIcon);
+    }
+    else{
+        
+        let notCompletedIcon = document.createElement('i');
+        notCompletedIcon.classList.add('iconNotComplete', 'icon', 'bi', 'bi-x-lg', 'fs-4');
+        notCompletedIcon.title = 'Mark as not completed';
+
+        col2.append(notCompletedIcon, deleteIcon);
+    }
+
+    row.append(col1, col2);
+
+    
+    if(task.isCompleted){
+        row.classList.add('completed');
+    }
+
+    return row;
+}
+
+function renderList(){
+    let activeTasks = taskList.filter((task)=> !task.isCompleted);
+    let completedTasks = taskList.filter((task)=> task.isCompleted);
+
+    // ACTIVE
+    let header2 = document.createElement('header');
+    let h2 = document.createElement('h2');
+    
+    let header3 = document.createElement('header');
+    let h3 = document.createElement('h3');
+
+    h2.classList.add('text-center','display-6', 'mb-3', 'text-sg', 'fw-bold');
+    h2.textContent = `YOUR TO-DO LIST`
+    header2.appendChild(h2);
+    wrapperList.appendChild(header2);
+    
+    activeTasks.forEach((task,index)=>{
+        wrapperList.appendChild(createTaskElement(task, index));
+    });
+
+    // COMPLETED
+    h3.classList.add('text-center','display-7', 'mb-3', 'mt-5', 'text-sg', 'fw-bold');
+    h3.textContent = `YOUR COMPLETED TASKS`
+
+    header3.appendChild(h3);
+    wrapperList.appendChild(header3);
+
+    completedTasks.forEach((task,index)=>{
+        wrapperList.appendChild(createTaskElement(task, index));
+    });
 };
 
 function showList() {
-    if(checkShow == false){
+    if(!checkShow){
         showTaskBtn.innerHTML = `Hide task list`
         checkShow = true;
         wrapperList.classList.remove('d-none');
@@ -72,10 +133,12 @@ function addNewTask(){
         addNewTaskInput.value = '';
         addNewTaskInput.placeholder = `Name of your new task`;
 
-        if(checkShow == true){
+        if(checkShow){
             renderList();
         }
-
+        else{
+            showList();
+        }
     }
     
 };
@@ -86,3 +149,10 @@ showTaskBtn.addEventListener('click', showList);
 
 // EVENT: ADD NEW TASK
 addNewTaskBtn.addEventListener('click', addNewTask);
+
+// EVENT: COMPLETE TASK 
+// EVENT: MODIFY TASK
+// EVENT: DELETE TASK
+
+// PLUS: ORDER BY NAME .sort()
+// PLUS: FILTER BY PRIORITY .filter()
